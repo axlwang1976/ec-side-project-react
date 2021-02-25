86f7802dbc339d2913e991f76face487b8e5b155
Axl Wang <axlwang1976@gmail.com>
2021-02-25 15:08:48 +0800

    first commit

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,42 @@
+import './HomePage.scss';
+
+const HomePage = () => {
+  return (
+    <div className="homeage">
+      <div className="menu-container">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HATS</h1>
+            <span className="subtutle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">JACKETS</h1>
+            <span className="subtutle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">SNEKERS</h1>
+            <span className="subtutle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">MEN</h1>
+            <span className="subtutle">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">WOMEN</h1>
+            <span className="subtutle">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
