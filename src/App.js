9902ded4ea20de1b68edaa7d1f8b4a112c30bef0
Axl Wang <axlwang1976@gmail.com>
2021-03-04 16:04:03 +0800

    create header component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/ShopPage/ShopPage';
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/shop" component={ShopPage} />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
+    </>
   );
 };
 
